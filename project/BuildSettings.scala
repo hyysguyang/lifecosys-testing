6267871ce1bb7aec821a02c4304dbf24380d734f
Young Gu <hyysguyang@gmail.com>
2016-08-23 13:48:49 +0800

    Update version.

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -7,13 +7,13 @@ import sbt.{IO, _}
   * @author <a href="mailto:Young.Gu@lifcosys.com">Young Gu</a>
   */
 object BuildSettings {
-  val VERSION = "0.1"
+  val VERSION = "0.2-SNAPSHOT"
 
   val lifecycle =
     addCommandAlias("install", ";formatJava;scalariformFormat;compile;test") ++
       addCommandAlias("testing", ";clean;scalariformFormat;compile;test")
 
-  lazy val projectBuildSettings = basicSettings ++ scalaFormattingSettings ++ publishSettings
+  lazy val projectBuildSettings = basicSettings ++ scalaFormattingSettings
 
   val basicSettings = Defaults.coreDefaultSettings ++ lifecycle ++ Seq(
     version := VERSION,
@@ -21,7 +21,7 @@ object BuildSettings {
     organization := "com.lifecosys",
     organizationHomepage := Some(new URL("https://lifecosys.com")),
     description := "Reusable testing utility.",
-    licenses += ("Apache 2.0", url("https://www.apache.org/licenses/LICENSE-2.0.html")),
+    licenses += ("Apache-2.0", url("https://www.apache.org/licenses/LICENSE-2.0.html")),
     startYear := Some(2016),
     scalaVersion := "2.11.6",
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
@@ -55,33 +55,5 @@ object BuildSettings {
       eclipseProfileFile in javaCodeFormatter := Some(codingStyle)
     )
   }
-
-  import bintray.BintrayKeys._
-  val publishSettings = Seq(
-    credentials += Credentials(Path.userHome / ".bintray" / ".credentials"),
-    publishMavenStyle := false,
-    bintrayOrganization in bintray := None,
-    pomExtra :=
-      <url>https://github.com/hyysguyang/java-code-formatter</url>
-        <licenses>
-          <license>
-            <name>Apache 2.0</name>
-            <url>http://www.apache.org/licenses/LICENSE-2.0</url>
-            <distribution>repo</distribution>
-          </license>
-        </licenses>
-        <scm>
-          <url>https://github.com/hyysguyang/java-code-formatter.git</url>
-          <connection>scm:https://github.com/hyysguyang/java-code-formatter.git</connection>
-        </scm>
-        <developers>
-          <developer>
-            <id>hyysguyang</id>
-            <name>Young Gu</name>
-            <url>https://plus.google.com/u/0/+YoungGu</url>
-          </developer>
-        </developers>
-  )
-
 
 }
